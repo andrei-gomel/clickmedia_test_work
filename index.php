b5209eece791fcc492a83d2f71e9fa5f1ab86af2
Андрей <84657319+andrei-gomel@users.noreply.github.com>
2022-11-12 10:49:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ session_start();
 
 include_once('functions.php');
 
+if (isset($_COOKIE['name'])) 
+	echo '<h3>Имя: ' . $_COOKIE['name'] . ' (через куки)</h3><br>';
+
 if(isset($_SESSION['name']))
 {
-	echo '<h3>Привет, ' . $_SESSION['name'] . '</h3>';
+	echo '<h3>Привет, ' . $_SESSION['name'] . ' (через сессию)</h3>';
 	echo ' <a href="index.php?action=logout">Выход</a>';
 }
 
